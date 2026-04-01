beef364f272f6410c33fe7d3b5842e21d28e2677
mantoangabe <mantoangabe@gmail.com>
2026-04-01 00:05:20 -0600

    hosting ready?

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99469e4c60add6b56fe9e6d4cc76cafec32467e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0f6f81dc733f7ec7f808377ecbc108a72e9ac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
